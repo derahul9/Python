260c7456a733ccbfc1cb224355464441c1eb35be
derahul9 <derahul9@gmail.com>
2020-04-10 16:47:32 -0600

    Netmiko

--- Netmiko/Netmiko_Test.py
+++ Netmiko/Netmiko_Test.py
@@ -2,13 +2,15 @@
 #If you set an invalid device type, then you'll get an error listing all the netmiko listed device types.
 
 from netmiko import ConnectHandler
-from inventory import cisco,cisco1                 #Importing a code that you have written, reusability of a python program
+import inventory
+#import inventory import *                   #This line imports all line space
+from inventory import cisco                 #Importing a code that you have written, reusability of a python program, Can be done in two ways as shown
 
 import logging                                     #importing netmiko logging
 logging.basicConfig(filename='test.log', level=logging.DEBUG)
 logger = logging.getLogger("netmiko")
 
-for device in (cisco, cisco1):
+for device in (cisco, inventory.cisco1):
     net_connect = ConnectHandler(**device)
     output = net_connect.send_command("show ver", expect_string=r'#') #This is the message you should get on the last line #
     print (output)
